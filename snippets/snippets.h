c9982baaf8aa1ec6ad120a3f2265b142289f2a15
NitrofMtl <NitrofMtl@users.noreply.github.com>
2016-03-28 14:35:34 -0400

    Added files via upload

---
+++ snippets/snippets.h
@@ -0,0 +1,93 @@
+/*
+  Copyright (c) 28/03/2016
+
+    By Nitrof
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy of
+  this software and associated documentation files (the "Software"), to deal in
+  the Software without restriction, including without limitation the rights to
+  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+  the Software, and to permit persons to whom the Software is furnished to do so,
+  subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+  
+#ifndef snippets_H
+#define snippets_H
+
+#if (ARDUINO >= 100)
+    #include "Arduino.h"
+#else
+    #include "WProgram.h"
+#endif
+#include <SD.h>
+
+#include <Ethernet.h>
+//#include <EthernetClient.h>
+#include <EthernetServer.h>
+#include <EthernetUdp.h>
+#include <TimeLib.h>
+
+#define LOCK 1
+
+
+//void getNTPTime();
+void setClock(EthernetUDP *Udp);
+
+time_t getNtpTime();
+void sendNTPpacket(IPAddress &address);
+void printDigits(int digits);
+void digitalClockDisplay();
+void printTime();
+void timeIp(IPAddress t_IP);
+void setTimeZone(int zone);
+
+
+
+String intToOnOff(byte inInt);
+String onOffBool(bool inBool);
+String toString(const char* inchar);
+bool stringToBool(const char* inChar);
+byte onOffToInt( const char* input);
+
+class Snippets {
+public:
+	int funcDelay(int delay);
+private:
+	int lastMillis = 0; //marker for lag correction delay
+};
+
+
+class Backup {
+public:
+	Backup(){};
+	void begin();
+	char* get();
+private:
+
+};
+
+class SetSchedule {
+public:
+	void interval(void (*function)(), int delay);
+	void timeOut(int delay);
+	void timeOut(int delay, bool lock);
+	void timeOut(void (*function)());
+	void reset();
+private:
+	unsigned int lastMillis = 0;
+	bool chkLOCK = 0;
+	unsigned int trigger = 0-1;
+};
+
+
+#endif
